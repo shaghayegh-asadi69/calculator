e3966b1cac504ac863b0d6dd5489550287d78f65
Shaghayegh <104257765+shaghayegh-asadi69@users.noreply.github.com>
2023-05-20 17:19:39 +0100

    Update index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const buttonsEl = document.querySelectorAll("button");
+const inputFieldEl = document.getElementById("result");
+
+for (let i = 0 ; i < buttonsEl.length; i++) {
+    buttonsEl[i].addEventListener("click", () => { const buttonValue = buttonsEl[i].textContent;
+        if(buttonValue === "c"){clearResult();
+        }
+        else if(buttonValue ==="=") {
+            calculateResult();
+        } else {
+            appendValue(buttonValue);
+        }       
+        });
+}
+function clearResult(){
+inputFieldEl.value = "";
+}
+function calculateResult(){
+    inputFieldEl.value = eval (inputFieldEl.value);
+}
+function appendValue(buttonValue){
+    inputFieldEl.value += buttonValue
+}
